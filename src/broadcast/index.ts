757b1864925ccd9f0795d09831d7271776bd5432
zhimao liu <zhimao_liu@icloud.com>
2018-05-25 16:28:21 -0700

    support more broadcast methods

--- src/broadcast/index.ts
+++ src/broadcast/index.ts
@@ -101,6 +101,110 @@ export default class Broadcast {
     );
   }
 
+  // post related
+  like(
+    username: string,
+    author: string,
+    weight: number,
+    post_id: string,
+    privKeyHex: string
+  ) {
+    const msg: LikeMsg = {
+      username,
+      weight,
+      author,
+      post_id
+    };
+    return this._broadcastTransaction(msg, _MSGTYPE.LikeMsgType, privKeyHex);
+  }
+
+  donate(
+    username: string,
+    author: string,
+    amount: string,
+    post_id: string,
+    from_app: string,
+    from_checking: boolean,
+    memo: string,
+    privKeyHex: string
+  ) {
+    const msg: DonateMsg = {
+      username,
+      amount,
+      author,
+      post_id,
+      from_app,
+      from_checking,
+      memo
+    };
+    return this._broadcastTransaction(msg, _MSGTYPE.DonateMsgType, privKeyHex);
+  }
+
+  reportOrUpvote(
+    username: string,
+    author: string,
+    post_id: string,
+    is_report: boolean,
+    privKeyHex: string
+  ) {
+    const msg: ReportOrUpvoteMsg = {
+      username,
+      author,
+      post_id,
+      is_report
+    };
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.ReportOrUpvoteMsgType,
+      privKeyHex
+    );
+  }
+
+  deletePost(author: string, post_id: string, privKeyHex: string) {
+    const msg: DeletePostMsg = {
+      author,
+      post_id
+    };
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.DeletePostMsgType,
+      privKeyHex
+    );
+  }
+
+  view(username: string, author: string, post_id: string, privKeyHex: string) {
+    const msg: ViewMsg = {
+      username,
+      author,
+      post_id
+    };
+    return this._broadcastTransaction(msg, _MSGTYPE.ViewMsgType, privKeyHex);
+  }
+
+  updatePost(
+    author: string,
+    title: string,
+    post_id: string,
+    content: string,
+    redistribution_split_rate: string,
+    links: IDToURLMapping[],
+    privKeyHex: string
+  ) {
+    const msg: UpdatePostMsg = {
+      author,
+      post_id,
+      title,
+      content,
+      links,
+      redistribution_split_rate
+    };
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.UpdatePostMsgType,
+      privKeyHex
+    );
+  }
+
   // validator related
   validatorDeposit(username: string, deposit: string, privKeyHex: string) {
     const msg: ValidatorDepositMsg = {
@@ -139,6 +243,20 @@ export default class Broadcast {
   }
 
   // vote related
+  vote(
+    voter: string,
+    proposal_id: string,
+    result: boolean,
+    privKeyHex: string
+  ) {
+    const msg: VoteMsg = {
+      voter,
+      proposal_id,
+      result
+    };
+    return this._broadcastTransaction(msg, _MSGTYPE.VoteMsgType, privKeyHex);
+  }
+
   voterDeposit(username: string, deposit: string, privKeyHex: string) {
     const msg: VoterDepositMsg = {
       username,
@@ -147,6 +265,139 @@ export default class Broadcast {
     return this._broadcastTransaction(
       msg,
       _MSGTYPE.VoterDepositMsgType,
+      privKeyHex
+    );
+  }
+
+  voterWithdraw(username: string, amount: string, privKeyHex: string) {
+    const msg: VoterWithdrawMsg = {
+      username,
+      amount
+    };
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.VoterWithdrawMsgType,
+      privKeyHex
+    );
+  }
+
+  voterRevoke(username: string, privKeyHex: string) {
+    const msg: VoterRevokeMsg = {
+      username
+    };
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.VoterRevokeMsgType,
+      privKeyHex
+    );
+  }
+
+  delegate(
+    delegator: string,
+    voter: string,
+    amount: string,
+    privKeyHex: string
+  ) {
+    const msg: DelegateMsg = {
+      delegator,
+      voter,
+      amount
+    };
+
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.DelegateMsgType,
+      privKeyHex
+    );
+  }
+
+  delegatorWithdraw(
+    delegator: string,
+    voter: string,
+    amount: string,
+    privKeyHex: string
+  ) {
+    const msg: DelegatorWithdrawMsg = {
+      delegator,
+      voter,
+      amount
+    };
+
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.DelegatorWithdrawMsgType,
+      privKeyHex
+    );
+  }
+
+  revokeDelegation(delegator: string, voter: string, privKeyHex: string) {
+    const msg: RevokeDelegationMsg = {
+      delegator,
+      voter
+    };
+
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.RevokeDelegationMsgType,
+      privKeyHex
+    );
+  }
+
+  developerRegister(username: string, deposit: string, privKeyHex: string) {
+    const msg: DeveloperRegisterMsg = {
+      username,
+      deposit
+    };
+
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.DeveloperRegisterMsgType,
+      privKeyHex
+    );
+  }
+
+  developerRevoke(username: string, privKeyHex: string) {
+    const msg: DeveloperRevokeMsg = {
+      username
+    };
+
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.DeveloperRevokeMsgType,
+      privKeyHex
+    );
+  }
+
+  grantDeveloper(
+    username: string,
+    authenticate_app: string,
+    validity_period: number,
+    grant_level: number,
+    privKeyHex: string
+  ) {
+    const msg: GrantDeveloperMsg = {
+      username,
+      authenticate_app,
+      validity_period,
+      grant_level
+    };
+
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.GrantDeveloperMsgType,
+      privKeyHex
+    );
+  }
+
+  providerReport(username: string, usage: number, privKeyHex: string) {
+    const msg: ProviderReportMsg = {
+      username,
+      usage
+    };
+
+    return this._broadcastTransaction(
+      msg,
+      _MSGTYPE.ProviderReportMsgType,
       privKeyHex
     );
   }
@@ -203,6 +454,73 @@ export interface CheckingToSavingMsg {
   amount: string;
 }
 
+// post related messages
+export interface CreatePostMsg {
+  PostCreateParams;
+}
+
+export interface PostCreateParams {
+  post_id: string;
+  title: string;
+  content: string;
+  author: string;
+  parent_author: string;
+  parent_postID: string;
+  source_author: string;
+  source_postID: string;
+  links: IDToURLMapping[];
+  redistribution_split_rate: string;
+}
+
+export interface IDToURLMapping {
+  identifier: string;
+  url: string;
+}
+
+export interface LikeMsg {
+  username: string;
+  weight: number;
+  author: string;
+  post_id: string;
+}
+
+export interface DonateMsg {
+  username: string;
+  amount: string;
+  author: string;
+  post_id: string;
+  from_app: string;
+  from_checking: boolean;
+  memo: string;
+}
+
+export interface ReportOrUpvoteMsg {
+  username: string;
+  author: string;
+  post_id: string;
+  is_report: boolean;
+}
+
+export interface DeletePostMsg {
+  author: string;
+  post_id: string;
+}
+
+export interface ViewMsg {
+  username: string;
+  author: string;
+  post_id: string;
+}
+
+export interface UpdatePostMsg {
+  author: string;
+  post_id: string;
+  title: string;
+  content: string;
+  links: IDToURLMapping[];
+  redistribution_split_rate: string;
+}
+
 // validator related messages
 export interface ValidatorDepositMsg {
   username: string;
@@ -225,6 +543,61 @@ export interface VoterDepositMsg {
   deposit: string;
 }
 
+export interface VoteMsg {
+  voter: string;
+  proposal_id: string;
+  result: boolean;
+}
+
+export interface VoterWithdrawMsg {
+  username: string;
+  amount: string;
+}
+
+export interface VoterRevokeMsg {
+  username: string;
+}
+
+export interface DelegateMsg {
+  delegator: string;
+  voter: string;
+  amount: string;
+}
+
+export interface DelegatorWithdrawMsg {
+  delegator: string;
+  voter: string;
+  amount: string;
+}
+
+export interface RevokeDelegationMsg {
+  delegator: string;
+  voter: string;
+}
+
+// developer related messages
+export interface DeveloperRegisterMsg {
+  username: string;
+  deposit: string;
+}
+
+export interface DeveloperRevokeMsg {
+  username: string;
+}
+
+export interface GrantDeveloperMsg {
+  username: string;
+  authenticate_app: string;
+  validity_period: number;
+  grant_level: number;
+}
+
+// infra related messages
+export interface ProviderReportMsg {
+  username: string;
+  usage: number;
+}
+
 const _MSGTYPE = {
   RegisterMsgType: '9E6F93EDF45140',
   TransferMsgType: '9E6F93EDF45140',
@@ -235,9 +608,29 @@ const _MSGTYPE = {
   SavingToCheckingMsgType: '9E6F93EDF45140',
   CheckingToSavingMsgType: '9E6F93EDF45140',
 
+  CreatePostMsgType: '9E6F93EDF45140',
+  LikeMsgType: '9E6F93EDF45140',
+  DonateMsgType: '9E6F93EDF45140',
+  ReportOrUpvoteMsgType: '9E6F93EDF45140',
+  DeletePostMsgType: '9E6F93EDF45140',
+  ViewMsgType: '9E6F93EDF45140',
+  UpdatePostMsgType: '9E6F93EDF45140',
+
   ValidatorDepositMsgType: '9E6F93EDF45140',
   ValidatorWithdrawMsgType: '9E6F93EDF45140',
   ValidatorRevokeMsgType: '9E6F93EDF45140',
 
-  VoterDepositMsgType: '9E6F93EDF45140'
+  VoteMsgType: '9E6F93EDF45140',
+  VoterDepositMsgType: '9E6F93EDF45140',
+  VoterWithdrawMsgType: '9E6F93EDF45140',
+  VoterRevokeMsgType: '9E6F93EDF45140',
+  DelegateMsgType: '9E6F93EDF45140',
+  DelegatorWithdrawMsgType: '9E6F93EDF45140',
+  RevokeDelegationMsgType: '9E6F93EDF45140',
+
+  DeveloperRegisterMsgType: '9E6F93EDF45140',
+  DeveloperRevokeMsgType: '9E6F93EDF45140',
+  GrantDeveloperMsgType: '9E6F93EDF45140',
+
+  ProviderReportMsgType: '9E6F93EDF45140'
 };
