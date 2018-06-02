a2f7b0ad9ce67d5a65caecd9b32277f8b99653ba
zhimao liu <zhimao_liu@icloud.com>
2018-06-02 11:54:08 -0700

    delete lib

--- lib/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { LINO } from './lino';
-import * as UTILS from './util';
-export { LINO, UTILS };
-export * from './common';
-export * from './broadcast';
-export * from './query';
-export * from './transport';
-export * from './util';
--- lib/util/index.d.ts
+++
@@ -1,5 +0,0 @@
-export declare function genPrivKeyHex(): string;
-export declare function pubKeyFromPrivate(privKeyHex: string): string;
-export declare function isValidUsername(username: string): boolean;
-export declare function isKeyMatch(privKeyHex: string, pubKeyHex: string): boolean;
-export declare function derivePrivKey(privKeyHex: any): string;
