87bda8a2a212ddc2cdb1b04e416657532f7ab113
Thomas Wang <wangxgwxg@gmail.com>
2018-06-02 14:11:10 -0700

    add message for push test script

--- push_test.sh
+++ push_test.sh
@@ -1,3 +1,4 @@
+echo "Please make sure you have pushed origin/dev, and on your local test branch now"
 git reset --hard origin/dev
 git reset --soft origin/test
 git checkout HEAD -- .gitignore push_test.sh
